e3cb67bdbfca48b88b8216f4fb9911837610d7b4
juarezrbr1 <juarezribeiro94@gmail.com>
2024-04-22 22:48:07 -0300

    Aula 68 - Filter, Map e Reduce

---
+++ Aula 68 - Filter + Map + Reduce/index.js
@@ -0,0 +1,13 @@
+// Soma do dobro dos valores pares
+
+// Filtra Pares
+// Dobra os valores
+// Reduz os valores
+
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numerosPares = numeros
+  .filter(valor => valor % 2 === 0)
+  .map(valor => valor * 2)
+  .reduce((acumulador, valor) => acumulador += valor);
+
+console.log(numerosPares);
