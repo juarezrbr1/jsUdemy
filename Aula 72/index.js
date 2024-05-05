6d6e0a304edca64c15b48ae29ec22336702833d4
juarezrbr1 <juarezribeiro94@gmail.com>
2024-05-05 19:05:36 -0300

    Aula 72

---
+++ Aula 72/index.js
@@ -0,0 +1,24 @@
+// defineProperty - defineProperties
+function Produto(nome, preco, estoque) {
+    this.nome = nome;
+    this.preco = preco;
+
+    let estoquePrivado = estoque;
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true, // mostra a chave
+        configurable: true, // configuravel
+        get: function() {
+            return estoquePrivado;
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') {
+                throw new TypeError('Mensagem');
+            }
+            estoquePrivado = valor;
+        }
+    });
+}
+
+const p1 = new Produto('Camiseta', 20, 3);
+p1.estoque = 5000;
+console.log(p1.estoque);
