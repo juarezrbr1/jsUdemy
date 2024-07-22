2b9e8bf65c426e77492a1cae9a805fa386f721a1
juarez <juarezribeiro94@gmail.com>
2024-07-21 23:07:38 -0300

    Login de usuario. Algo de errado com a validação de login

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -1,28 +1,60 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
+    console.log(req.session.user)
+    if (req.session.user) return res.render('login-logado');
     res.render('login');
 };
 
-exports.register = async function(req, res) {
+exports.register = async function (req, res) {
     try {
         const login = new Login(req.body);
         await login.register();
-    
-        if(login.errors.length > 0) {
+
+        if (login.errors.length > 0) {
             req.flash('errors', login.errors);
-            req.session.save(function() {
+            req.session.save(function () {
                 console.log(login.errors);
                 return res.redirect('/login/index');
             });
             return;
         }
         req.flash('success', 'Seu usuário foi criado com sucesso.');
-            req.session.save(function() {
-                return res.redirect('/login/index');
-            });
-    } catch(e) {
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+    } catch (e) {
         console.log(e);
         return res.render('404');
     }
+};
+
+exports.login = async function (req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+
+        if (login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function () {
+                // console.log(login.errors);
+                return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema.');
+        req.session.user = login.user;
+        req.session.save(function () {
+            return res.redirect('/login/index');
+        });
+    } catch (e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
+
+exports.logout = function (req, res) {
+    req.session.destroy();
+    res.redirect('/');
 };
