5aa97484095515d085f2cac9ba79271bb1687212
juarez <juarezribeiro94@gmail.com>
2024-07-22 20:04:45 -0300

    Estruturando rotas e controllers

---
+++ api_rest/server.js
@@ -0,0 +1,8 @@
+import app from './app.js'
+
+
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    console.log(`CTRL + Clique http://localhost:${port}`);
+})
