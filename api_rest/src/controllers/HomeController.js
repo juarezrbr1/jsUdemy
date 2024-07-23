bda8eba7316d4fc2753d7bb95a2b074a146c34c0
juarez <juarezribeiro94@gmail.com>
2024-07-23 19:03:58 -0300

    Crud Alunos

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -2,15 +2,7 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Juarez',
-      sobrenome: 'Ribeiro',
-      email: 'juarez@email.com',
-      idade: 30,
-      peso: 70,
-      altura: 1.75,
-    });
-    res.json(novoAluno);
+    res.json('Index');
   }
 }
 
