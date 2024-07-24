33f248fb9c0e97cf27ba928f87b6140e6434795b
juarez <juarezribeiro94@gmail.com>
2024-07-24 00:09:59 -0300

    Salvando dados das fotos na base de dados

---
+++ api_rest/src/database/migrations/20240724023002-criar-tabela-de-foto-do-aluno.js
@@ -0,0 +1,38 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('fotos', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    originalname: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    filename: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    aluno_id: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'alunos',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+  down: (queryInterface) => queryInterface.dropTable('fotos'),
+};
