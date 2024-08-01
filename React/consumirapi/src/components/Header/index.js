ccf8ceaf422c3aae88a8da3b6d5ead62b8b4ccb2
juarez <juarezribeiro94@gmail.com>
2024-08-01 20:45:38 -0300

    Criada pasta do projeto real

---
+++ React/consumirapi/src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux';
+
+import { Nav } from './styled';
+
+export default function Header() {
+  const botaoClicado = useSelector(state => state.example.botaoClicado);
+
+  return (
+  <Nav>
+    <Link to="/">
+      <FaHome size={24} />
+    </Link>
+    <Link to="/login">
+      <FaSignInAlt size={24} />
+    </Link>
+    <Link to="/asd">
+      <FaUserAlt size={24} />
+    </Link>
+    {botaoClicado ? <h1>Botão foi clicado</h1> : <h1>Não clicado</h1>}
+  </Nav>
+  )
+}
