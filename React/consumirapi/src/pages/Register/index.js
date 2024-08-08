0a7b2722d8bfaee3fff7a1504c452697d8fcbb53
juarez <juarezribeiro94@gmail.com>
2024-08-08 20:33:20 -0300

    Componente Loading

--- React/consumirapi/src/pages/Register/index.js
+++ React/consumirapi/src/pages/Register/index.js
@@ -7,11 +7,13 @@ import { Container } from "../../styles/GlobalStyles";
 import { Form } from "./styled";
 import axios from '../../services/axios'
 import history from '../../services/history'
+import Loading from '../../components/Loading';
 
 export default function Register() {
   const [nome, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -31,6 +33,8 @@ export default function Register() {
     }
     if(formErrors) return;
 
+    setIsLoading(true);
+
     try {
       await axios.post('/users', {
         nome,
@@ -38,16 +42,19 @@ export default function Register() {
         email,
       })
       toast.success('Você fez seu cadastro');
+      setIsLoading(false);
       history.push('/login/');
     } catch (err) {
       const errors = get(err, 'response.data.errors', []);
 
       errors.map(error => toast.error(error));
+      setIsLoading(false);
     }
   }
 
   return (
     <Container>
+      <Loading isLoading={isLoading} />
       <h1>Crie sua conta</h1>
       <Form onSubmit={handleSubmit}>
         <label htmlFor="nome">
