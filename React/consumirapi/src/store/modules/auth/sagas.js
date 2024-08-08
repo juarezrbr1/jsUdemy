05adeb4745058f6e38a30203a84a1c02542dd963
juarez <juarezribeiro94@gmail.com>
2024-08-07 23:20:27 -0300

    LOGIN

---
+++ React/consumirapi/src/store/modules/auth/sagas.js
@@ -0,0 +1,36 @@
+import { call, put, all, takeLatest } from "redux-saga/effects";
+import { get } from 'lodash';
+import * as actions from "./actions";
+import * as types from "../types";
+import axios from "../../../services/axios";
+import history from "../../../services/history";
+import { toast } from "react-toastify";
+
+function* loginRequest({ payload }) {
+  try {
+    const response = yield call(axios.post, "/tokens", payload);
+    yield put(actions.loginSuccess({ ...response.data }));
+
+    toast.success("Login efetuado com sucesso");
+
+    axios.defaults.headers.Authorization = `Bearer ${response.data.token}`;
+
+    history.push(payload.prevPath);
+  } catch (e) {
+    toast.error("Usuário ou senha inválidos");
+    yield put(actions.loginFailure());
+  }
+}
+
+function persistRehydrate({ payload }) {
+  const token = get(payload, "auth.token", "");
+  if (!token) return;
+  axios.defaults.headers.Authorization = `Bearer ${token}`;
+
+  // yield put(actions.loginSuccess({ token }));
+}
+
+export default all([
+  takeLatest(types.LOGIN_REQUEST, loginRequest),
+  takeLatest(types.PERSIST_REHYDRATE, persistRehydrate),
+]);
--- React/consumirapi/src/store/modules/rootReducer.js
+++ React/consumirapi/src/store/modules/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
-import exampleReducer from './example/reducer';
+import auth from './auth/reducer';
 
 export default combineReducers({
-  // Add reducers here
-  example: exampleReducer,
+  auth,
 });
