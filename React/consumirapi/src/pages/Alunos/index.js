9595185ae2a07c720097627d27dd2a3e2c70e520
juarez <juarezribeiro94@gmail.com>
2024-08-13 22:03:29 -0300

    excluindo alunos

--- React/consumirapi/src/pages/Alunos/index.js
+++ React/consumirapi/src/pages/Alunos/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { get } from "lodash";
-import { FaUserCircle, FaEdit, FaTrashAlt } from "react-icons/fa";
+import {
+  FaUserCircle,
+  FaEdit,
+  FaTrashAlt,
+  FaExclamation,
+} from "react-icons/fa";
 
 import { Container } from "../../styles/GlobalStyles";
 import { AlunoContainer, ProfilePicture } from "./styled";
 import axios from "../../services/axios";
 
-import Loading from '../../components/Loading';
+import Loading from "../../components/Loading";
+import { toast } from "react-toastify";
 
 export default function Alunos() {
   const [alunos, setAlunos] = useState([]);
@@ -23,13 +29,43 @@ export default function Alunos() {
 
     getdata();
   }, []);
+
+  const handleDeleteAsk = (e) => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute("display", "block");
+    e.currentTarget.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+
+    try {
+      setIsLoading(true);
+      await axios.delete(`/alunos/${id}`);
+      const novosAlunos = [...alunos];
+      novosAlunos.splice(index, 1);
+      setAlunos(novosAlunos);
+      setIsLoading(false);
+    } catch (err) {
+      const status = get(err, "response.status", 0);
+
+      if (status === 401) {
+        toast.error("Você precisa fazer login.");
+      } else {
+        toast.error("Erro ao excluir aluno");
+      }
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Alunos</h1>
 
       <AlunoContainer>
-        {alunos.map((aluno) => (
+        {alunos.map((aluno, index) => (
           <div key={String(aluno.id)}>
             <ProfilePicture>
               {get(aluno, "Fotos[0].url", false) ? (
@@ -43,9 +79,16 @@ export default function Alunos() {
             <Link to={`/alunos/${aluno.id}/editar`}>
               <FaEdit size={16} />
             </Link>
-            <Link to={`/alunos/${aluno.id}/delete`}>
+            <Link onClick={handleDeleteAsk} to={`/alunos/${aluno.id}/delete`}>
               <FaTrashAlt sinze={16} />
             </Link>
+
+            <FaExclamation
+            size={16}
+            display="none"
+            cursor="pointer"
+            onClick={e => handleDelete(e, aluno.id, index)}
+             />
           </div>
         ))}
       </AlunoContainer>
